9f9c1a89c68bb6345dafd03b5a35d79505c30ffa
rvoidex7 <70305889+yakupozcan@users.noreply.github.com>
2025-09-22 23:38:28 +0300

    first commit

---
+++ Snake87.ConsoleApp/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Snake87.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ReadKey();
+        }
+    }
+}
